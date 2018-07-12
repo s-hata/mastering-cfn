d4063d43b87c1cc837db6d432f097484b743b448
s-hata <shinichi.hatano@ctc-g.co.jp>
2018-07-13 00:33:01 +0900

    fix

--- continuous-deploy-01/scripts/deploy.sh
+++ continuous-deploy-01/scripts/deploy.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+STACK_NAME="mastering-cfn-continuous-deploy-01"
+
 aws s3 sync . s3://ignite-cfn-template-store3/examples/mastering-cfn/continuous-deploy-01
 
-
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --template-url https://s3.amazonaws.com/ignite-cfn-template-store3/examples/mastering-cfn/continuous-deploy-01/pipeline/src/cft/root.yml \
+  --on-failure DO_NOTHING \
+  --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation wait \
+  stack-create-complete \
+  --stack-name $STACK_NAME
