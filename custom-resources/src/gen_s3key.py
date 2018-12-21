bc8da2f8fc98aa86b3396e819de9050d49441628
s-hata <shinichi.hatano@ctc-g.co.jp>
2018-12-21 13:18:18 +0900

    modified the custome resource

--- custom-resources/src/gen_s3key.py
+++ custom-resources/src/gen_s3key.py
@@ -6,61 +6,73 @@ import urllib.request
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
+
 def handler(event, context):
     signal.alarm((context.get_remaining_time_in_millis() // 1000) - 1)
     try:
-        LOGGER.info(f'REQUEST RECEIVED[event]:\n ${event}')
-        LOGGER.info(f'REQUEST RECEIVED[context]:\n ${context}')
-        if event['RequestType'] == 'Create':
-            LOGGER.info('CREATE!')
-            send_response(event, context, "SUCCESS",
-                          {"Message": "Resource creation successful!"})
-        elif event['RequestType'] == 'Update':
-            LOGGER.info('UPDATE!')
-            send_response(event, context, "SUCCESS",
-                          {"Message": "Resource update successful!"})
-        elif event['RequestType'] == 'Delete':
-            LOGGER.info('DELETE!')
-            send_response(event, context, "SUCCESS",
-                          {"Message": "Resource deletion successful!"})
+        LOGGER.info(f"REQUEST RECEIVED[event]:\n ${event}")
+        LOGGER.info(f"REQUEST RECEIVED[context]:\n ${context}")
+        if event["RequestType"] == "Create":
+            create_handler(event, context)
+        elif event["RequestType"] == "Update":
+            update_handler(event, context)
+        elif event["RequestType"] == "Delete":
+            delete_handler(event, context)
         else:
-            LOGGER.info('FAILED!')
+            LOGGER.info("FAILED!")
             send_response(event, context, "FAILED",
                           {"Message": "Unexpected event received from CloudFormation"})
-    except: #pylint: disable=W0702
-        LOGGER.info('FAILED!')
-        send_response(event, context, "FAILED", {
-            "Message": "Exception during processing"})
+    except:
+        LOGGER.info("FAILED!")
+        send_response(event, context, "FAILED", {"Message": "Exception during processing"})
+    finally:
+        signal.alarm(0)
 
 
 def send_response(event, context, response_status, response_data):
-    '''Send a resource manipulation status response to CloudFormation'''
     response_body = json.dumps({
         "Status": response_status,
         "Reason": "See the details in CloudWatch Log Stream: " + context.log_stream_name,
         "PhysicalResourceId": context.log_stream_name,
-        "StackId": event['StackId'],
-        "RequestId": event['RequestId'],
-        "LogicalResourceId": event['LogicalResourceId'],
+        "StackId": event["StackId"],
+        "RequestId": event["RequestId"],
+        "LogicalResourceId": event["LogicalResourceId"],
         "Data": response_data
     }).encode("utf-8")
 
-    LOGGER.info('ResponseURL: %s', event['ResponseURL'])
-    LOGGER.info('ResponseBody: %s', response_body)
+    LOGGER.info(f"ResponseURL: {event['ResponseURL']}")
+    LOGGER.info(f"ResponseBody: {response_body}")
 
     opener = urllib.request.build_opener(urllib.request.HTTPHandler)
-    request = urllib.request.Request(event['ResponseURL'], data=response_body)
-    request.add_header('Content-Type', '')
-    request.add_header('Content-Length', len(response_body))
-    request.get_method = lambda: 'PUT'
+    request = urllib.request.Request(event["ResponseURL"], data=response_body)
+    request.add_header("Content-Type", "")
+    request.add_header("Content-Length", len(response_body))
+    request.get_method = lambda: "PUT"
     response = opener.open(request)
-    LOGGER.info("Status code: %s", response.getcode())
-    LOGGER.info("Status message: %s", response.msg)
+    LOGGER.info(f"Status code: {response.getcode()}")
+    LOGGER.info(f"Status message: {response.msg}")
+
+
+def create_handler(event, context):
+    LOGGER.info("CREATE!")
+    send_response(event, context, "SUCCESS",
+                  {"Message": "Resource creation successful!"})
+
+
+def update_handler(event, context):
+    LOGGER.info("UPDATE!")
+    send_response(event, context, "SUCCESS",
+                  {"Message": "Resource update successful!"})
+
+
+def delete_handler(event, context):
+    LOGGER.info("DELETE!")
+    send_response(event, context, "SUCCESS",
+                  {"Message": "Resource deletion successful!"})
 
 
 def timeout_handler(_signal, _frame):
-    '''Handle SIGALRM'''
-    raise Exception('Time exceeded')
+    raise Exception("Time exceeded")
 
 
 signal.signal(signal.SIGALRM, timeout_handler)
